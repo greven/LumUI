95deeb7460a910a356ef26df40feaf668bbe3b56
Nuno M <greven.pt@gmail.com>
2018-04-15 11:13:32 +0100

    Create cooldowns modules

---
+++ mods/cooldownsCount.lua
@@ -0,0 +1,154 @@
+-- --------------------------------------------
+-- Credits: AltzUI.
+-- --------------------------------------------
+
+local _, ns = ...
+
+local L, C, G = unpack(select(2, ...))
+
+-- ---------------------------------
+-- > Variables
+-- ---------------------------------
+
+local fontSize = 16
+
+-- 'Basic' version of OmniCC
+local format, floor, GetTime = string.format, math.floor, GetTime
+local Multiplier = 0.8
+
+local function GetFormattedTime(s)
+	if s>3600 then
+		return format("%dh", floor(s/3600 + 0.5)), s % 3600
+	elseif s>60 then
+		return format("%dm", floor(s/60 + 0.5)), s % 60
+	elseif s>8 then
+		return floor(s + 0.5), s - floor(s)
+	elseif s>3 then
+		return format("|cFFEEEE00%d|r", floor(s + 0.5)), s - floor(s)
+	else
+		return format("|cFFFF2400%.1f|r", s), 0.05
+	end
+end
+
+local function Timer_OnUpdate(self, elapsed)
+	if self.text:IsShown() then
+		if self.nextUpdate>0 then
+			self.nextUpdate = self.nextUpdate - elapsed
+		else
+			local remain = self.duration - (GetTime() - self.start)
+			if floor(remain + 0.5) > 0 then
+				local time, nextUpdate = GetFormattedTime(remain)
+				self.text:SetText(time)
+				self.nextUpdate = nextUpdate
+			else
+				self.text:Hide()
+			end
+		end
+	end
+end
+
+local methods = getmetatable(ActionButton1Cooldown).__index
+hooksecurefunc(methods, "SetCooldown", function(self, start, duration)
+	if self.noshowcd then return end
+	local parent = self:GetParent()
+	if parent then
+		local parent_name = parent:GetName()
+		if parent_name and parent_name:find("CompactRaidFrame") then
+			return
+		end
+	end
+	
+	if (self:GetWidth() >= 15) and (self:GetHeight() >= 15) then
+		local s, d = tonumber(start), tonumber(duration)
+		if s and d then
+			if s > 0 and d > 2.5 then	
+				self.start = s
+				self.duration = d
+				self.nextUpdate = 0
+		
+				if (self:GetWidth() >= 25) and (self:GetHeight() >= 25) then
+          if not self.text then
+						self.text = L:createNumber(self, "OVERLAY", fontSize, "OUTLINE", "CENTER")
+						self.text:SetTextColor(.4, .95, 1)
+						self.text:SetPoint("CENTER", 0, 0)					
+					else
+						self.text:SetFont(G.numFont, fontSize, "OUTLINE")
+					end
+				else
+					if not self.text then
+						self.text = L:createNumber(self, "OVERLAY", self:GetWidth()*.7+1, "OUTLINE", "CENTER")
+						self.text:SetTextColor(.4, .95, 1)
+						self.text:SetPoint("CENTER", 0, 0)						
+					else
+						self.text:SetFont(G.numFont, self:GetWidth()*.7+1, "OUTLINE")
+					end
+				end
+				
+				if not self:GetScript("OnUpdate") then
+					self:SetScript("OnUpdate", Timer_OnUpdate)
+				end
+				
+				self.text:Show()
+				
+			elseif self.text then
+				self.text:Hide()
+			end
+		end
+	elseif self.text then
+		if start>0 and duration>2.5 then
+			self.start = start
+			self.duration = duration
+			self.nextUpdate = 0
+			if not self:GetScript("OnUpdate") then
+				self:SetScript("OnUpdate", Timer_OnUpdate)
+				self.text:Show()
+			end
+		else
+			self.text:Hide()
+		end
+	end
+end)
+
+local hooked = {}
+local active = {}
+
+local cooldownCount = CreateFrame('Frame')
+cooldownCount:Hide()
+
+cooldownCount:SetScript('OnEvent', function(self, event)
+	if event == 'ACTIONBAR_UPDATE_COOLDOWN' then
+		for cooldown in pairs(active) do
+			local button = cooldown:GetParent()
+			local start, duration, enable = GetActionCooldown(button.action)
+			cooldown:SetCooldown(start, duration)
+		end
+	else
+		SetCVar("countdownForCooldowns", 0)
+	end
+end)
+
+cooldownCount:RegisterEvent('ACTIONBAR_UPDATE_COOLDOWN')
+cooldownCount:RegisterEvent('PLAYER_LOGIN')
+
+local function cooldown_OnShow(self)
+	active[self] = true
+    end
+
+local function cooldown_OnHide(self)
+	active[self] = nil
+end
+
+local function actionButton_Register(frame)
+	local cooldown = frame.cooldown
+	if not hooked[cooldown] then
+		cooldown:HookScript('OnShow', cooldown_OnShow)
+		cooldown:HookScript('OnHide', cooldown_OnHide)
+		hooked[cooldown] = true
+	end
+end
+
+for i, frame in pairs(ActionBarButtonEventsFrame.frames) do
+	actionButton_Register(frame)
+end
+
+hooksecurefunc('ActionBarButtonEventsFrame_RegisterFrame', actionButton_Register)
---
+++ mods/cooldownsPulse.lua
@@ -0,0 +1,249 @@
+-- --------------------------------------------
+-- Credits: Doom_CooldownPulse
+-- --------------------------------------------
+
+local _, ns = ...
+
+local L, C, G = unpack(select(2, ...))
+
+-- ---------------------------------
+-- > Config
+-- ---------------------------------
+
+local size = 36
+local posX, posY = 0, 150
+local fontSize = 10
+
+local showSpellName = true
+local fadeInTime = 0.3
+local fadeOutTime = 0.7
+local holdTime = 0.1
+local maxAlpha = 1.0
+local animScale = 1.25
+
+-- Spells to Ignore
+local blacklist = {
+
+}
+
+-- Threshold to watch spells (seconds)
+local thresholdByClass = {
+	DEATHKNIGHT = 10,
+	DRUID = 5,
+	HUNTER = 5, 
+	MAGE = 5,
+	MONK = 5,
+	PALADIN = 5,
+	PRIEST = 3,
+	ROGUE = 5,
+	SHAMAN = 5,
+	WARLOCK = 5,
+	WARRIOR = 5
+}
+
+local threshold = thresholdByClass[G.playerClass] or 3.0
+
+-- ---------------------------------
+
+local cooldowns, animating, watching, ignoredSpells = { }, { }, { }, { }
+local GetTime = GetTime
+
+local f = CreateFrame('Frame', 'lumCDA')
+f:SetScript('OnEvent', function(self, event, ...) self[event](self, ...) end)
+
+f:SetSize(size, size)
+f:SetPoint('CENTER', UIParent,'CENTER', posX, posY)
+
+f.text = L:createText(f, 'ARTWORK', fontSize, 'OUTLINE', 'CENTER')
+f.text:SetTextColor(1, 1, 1)
+f.text:SetShadowOffset(1,-1)
+f.text:SetShadowColor(0, 0, 0, 1)
+f.text:SetPoint('TOP', f, 'BOTTOM', 1, -6)
+
+f.icon = f:CreateTexture(nil, 'BACKGROUND')
+f.icon:SetTexCoord(.08, .92, .08, .92)
+f.icon:SetAllPoints(f)
+
+f.border = L:CreatePanel(true, true, 'lumCDA', 'lumCDA', f:GetWidth() + 6, f:GetHeight() + 6, 
+	{{'TOPLEFT', f, 'TOPLEFT', -3, 3}}, 32, 8, 0, 0.6)
+f.border:Hide()
+
+-- Utils
+local function tcount(tab)
+	local n = 0
+	for _ in pairs(tab) do
+			n = n + 1
+	end
+	return n
+end
+
+local function setIgnoredSpells()
+	for i=1, #blacklist do
+		ignoredSpells[strtrim(blacklist[i])] = true
+	end
+end
+
+-- OnUpdate
+local elapsed = 0
+local runtimer = 0
+
+local function OnUpdate(_, update)
+	elapsed = elapsed + update
+	if (elapsed > 0.05) then
+		for i,v in pairs(watching) do
+			if (GetTime() >= v[1] + 0.5) then
+				local start, duration, enabled, texture, isPet, name
+				if (v[2] == "spell") then
+					name = GetSpellInfo(v[3])
+					texture = GetSpellTexture(v[3])
+					start, duration, enabled = GetSpellCooldown(v[3])
+				elseif (v[2] == "item") then
+					name = GetItemInfo(i)
+					texture = v[3]
+					start, duration, enabled = GetItemCooldown(i)
+				-- elseif (v[2] == "pet") then
+				-- 	name, _, texture = GetPetActionInfo(v[3])
+				-- 	start, duration, enabled = GetPetActionCooldown(v[3])
+				-- 	isPet = true
+				end
+
+				if ignoredSpells[name] then
+						watching[i] = nil
+				else
+					if (enabled ~= 0) then
+							if (duration and duration > threshold and texture) then
+									cooldowns[i] = { start, duration, texture, isPet, name }
+							end
+					end
+					if (not (enabled == 0 and v[2] == "spell")) then
+							watching[i] = nil
+					end
+				end
+			end
+		end
+
+		for i,v in pairs(cooldowns) do
+				local remaining = v[2]-(GetTime()-v[1])
+				if (remaining <= 0) then
+						tinsert(animating, {v[3],v[4],v[5]})
+						cooldowns[i] = nil
+				end
+		end
+			
+		elapsed = 0
+		if (#animating == 0 and tcount(watching) == 0 and tcount(cooldowns) == 0) then
+				f:SetScript("OnUpdate", nil)
+				return
+		end
+	end
+
+	if (#animating > 0) then
+		runtimer = runtimer + update
+		if (runtimer > (fadeInTime + holdTime + fadeOutTime)) then
+				tremove(animating,1)
+				runtimer = 0
+				f.text:SetText(nil)
+				f.icon:SetTexture(nil)
+				f.icon:SetVertexColor(1,1,1)
+				f.border:Hide()
+		else
+				if (not f.icon:GetTexture()) then
+						if (animating[1][3] ~= nil and showSpellName) then
+							f.text:SetText(animating[1][3])
+						end
+						f.icon:SetTexture(animating[1][1])
+						f.border:Show()
+						if animating[1][2] then
+							f.icon:SetVertexColor(unpack({1, 1, 1}))
+						end
+						-- PlaySoundFile("Interface\\AddOns\\Doom_CooldownPulse\\lubdub.wav")
+				end
+				local alpha = maxAlpha
+				if (runtimer < fadeInTime) then
+						alpha = maxAlpha * (runtimer / fadeInTime)
+				elseif (runtimer >= fadeInTime + holdTime) then
+						alpha = maxAlpha - ( maxAlpha * ((runtimer - holdTime - fadeInTime) / fadeOutTime))
+				end
+				f:SetAlpha(alpha)
+				local scale = size+(size*((animScale-1)*(runtimer/(fadeInTime+holdTime+fadeOutTime))))
+				f:SetWidth(scale)
+				f:SetHeight(scale)
+				f.border:SetWidth(scale + 6)
+				f.border:SetHeight(scale + 6)
+		end
+	end
+end
+
+-- Events
+function f:ADDON_LOADED(addon)
+	setIgnoredSpells()
+	self:UnregisterEvent("ADDON_LOADED")
+end
+f:RegisterEvent("ADDON_LOADED")
+
+
+
+function f:UNIT_SPELLCAST_SUCCEEDED(unit, spell, rank, lineID, spellID)
+	if (unit == 'player') then
+		watching[spellID] = {GetTime(), 'spell', spellID}
+		if (not self:IsMouseEnabled()) then
+				self:SetScript("OnUpdate", OnUpdate)
+		end
+	end
+end
+f:RegisterEvent('UNIT_SPELLCAST_SUCCEEDED')
+
+function f:PLAYER_ENTERING_WORLD()
+	local inInstance, instanceType = IsInInstance()
+	if (inInstance and instanceType == "arena") then
+			self:SetScript("OnUpdate", nil)
+			wipe(cooldowns)
+			wipe(watching)
+	end
+end
+f:RegisterEvent("PLAYER_ENTERING_WORLD")
+
+-- function f:COMBAT_LOG_EVENT_UNFILTERED(...)
+-- 	local _,event,_,_,_,sourceFlags,_,_,_,_,_,spellID = ...
+-- 	if (event == "SPELL_CAST_SUCCESS") then
+-- 			if (bit.band(sourceFlags,COMBATLOG_OBJECT_TYPE_PET) == COMBATLOG_OBJECT_TYPE_PET and bit.band(sourceFlags,COMBATLOG_OBJECT_AFFILIATION_MINE) == COMBATLOG_OBJECT_AFFILIATION_MINE) then
+-- 					local name = GetSpellInfo(spellID)
+-- 					local index = GetPetActionIndexByName(name)
+-- 					if (index and not select(7,GetPetActionInfo(index))) then
+-- 							watching[spellID] = {GetTime(),"pet",index}
+-- 					elseif (not index and spellID) then
+-- 							watching[spellID] = {GetTime(),"spell",spellID}
+-- 					else
+-- 							return
+-- 					end
+-- 					if (not self:IsMouseEnabled()) then
+-- 							self:SetScript("OnUpdate", OnUpdate)
+-- 					end
+-- 			end
+-- 	end
+-- end
+-- f:RegisterEvent("COMBAT_LOG_EVENT_UNFILTERED")
+
+hooksecurefunc("UseAction", function(slot)
+	local actionType,itemID = GetActionInfo(slot)
+	if (actionType == "item") then
+			local texture = GetActionTexture(slot)
+			watching[itemID] = {GetTime(),"item",texture}
+	end
+end)
+
+hooksecurefunc("UseInventoryItem", function(slot)
+	local itemID = GetInventoryItemID("player", slot);
+	if (itemID) then
+			local texture = GetInventoryItemTexture("player", slot)
+			watching[itemID] = {GetTime(),"item",texture}
+	end
+end)
+
+hooksecurefunc("UseContainerItem", function(bag,slot)
+	local itemID = GetContainerItemID(bag, slot)
+	if (itemID) then
+			local texture = select(10, GetItemInfo(itemID))
+			watching[itemID] = {GetTime(),"item",texture}
+	end
+end)
